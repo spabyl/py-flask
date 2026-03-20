5e893df6ad4ba3112daf81a4a881bd6fe0dabae7
PCMA-SOFT <pacema1@gmail.com>
2026-03-20 09:36:02 -0300

    Update: 20-03-2026  9:36

--- _pyinstaller.bat
+++ _pyinstaller.bat
@@ -1,6 +1,15 @@
 @echo off
 cls
-pyinstaller app/app.py --add-data "app/data.py;." --add-data "app/templates;templates" --add-data "app/static;static" --onefile --optimize 2 --clean -i ./win_ico.ico  -y -n local_flask 
+pyinstaller app/app.py ^
+--add-data "app/data.py;." ^
+--add-data "app/templates;templates" ^
+--add-data "app/static;static" ^
+--onefile ^
+--optimize 2 ^
+--clean ^
+-w ^
+-i ./win_ico.ico  ^
+-y -n local_flask 
 
 echo.
 echo ============
